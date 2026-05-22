01ec04a5074402d73b24aed7d58218537deb4695
Caezer <caezer@birddreamer.com>
2026-05-22 14:53:18 +0100

    correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from "next";
 import { Poppins, } from "next/font/google";
 import "./globals.css";
 import { romantic, Romancio, aroba, clairo, alrevo } from "./components/font";
-import { Suspense } from "react";
-import Loading from "./loading";
-import LoadingScreen from "./loading";
+
 import Providers from "./components/Providers";
 
  
